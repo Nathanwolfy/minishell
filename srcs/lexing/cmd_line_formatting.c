8cec3f605e283be9889a6814f63e4a77b351af18
Elliosante <ellio.hickman001@gmail.com>
2024-02-09 18:10:11 +0100

    fixed variable expansion issue

--- srcs/lexing/cmd_line_formatting.c
+++ srcs/lexing/cmd_line_formatting.c
@@ -6,34 +6,50 @@
 /*   By: nlederge <nlederge@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/02 09:27:54 by ehickman          #+#    #+#             */
-/*   Updated: 2024/02/09 15:18:36 by ehickman         ###   ########.fr       */
+/*   Updated: 2024/02/09 18:09:22 by ehickman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "common.h"
 
-static void	replace_dquote_loop(char *line, int *flags, int *i, char **envp)
+static void	get_exit_status_len(int *flags, int *i, int exit_status)
 {
-	flags[*i] = 1;
-	line[*i] = DQUOTE;
-	*i += 1;
-	while (line[*i])
+	if (exit_status == 0)
+		flags[*i] = -1;
+	else
 	{
-		if (line[*i] == '\"')
+		while (exit_status > 0)
 		{
-			line[*i] = DQUOTE;
-			flags[*i] = 1;
+			flags[*i] -= 1;
+			exit_status /= 10;
+		}
+	}
+	*i += 2;
+}
+
+static void	replace_dquote_loop(int *i, char **envp, t_cmd_line_info *d)
+{
+	d->flags[*i] = 1;
+	d->line[*i] = DQUOTE;
+	*i += 1;
+	while (d->line[*i])
+	{
+		if (d->line[*i] == '\"')
+		{
+			d->line[*i] = DQUOTE;
+			d->flags[*i] = 1;
 			*i += 1;
 			return ;
 		}
-		else if (line[*i] == '$')
-		{
-			get_env_len(line, flags, i, envp);
-			continue ;
-		}
+		if (d->line[*i] == '$' && d->line[*i + 1] == '?')
+			get_exit_status_len(d->flags, i, d->exit_status);
+		else if (d->line[*i] == '$')
+			get_env_len(d->line, d->flags, i, envp);
 		else
-			flags[*i] = 1;
-		*i += 1;
+		{
+			d->flags[*i] = 1;
+			*i += 1;
+		}
 	}
 }
 
@@ -55,41 +71,26 @@ static void	replace_squote_loop(char *line, int *flags, int *i)
 	}
 }
 
-static void	get_exit_status_len(int *flags, int *i, int exit_status)
-{
-	if (exit_status == 0)
-		flags[*i] = -1;
-	else
-	{
-		while (exit_status > 0)
-		{
-			flags[*i] -= 1;
-			exit_status /= 10;
-		}
-	}
-	*i += 2;
-}
-
-static void	lexer_set_flags(char *line, int *flags, char **envp, int exit_status)
+static void	lexer_set_flags(char **envp, t_cmd_line_info *d)
 {
 	int	i;
 	int	len;
 
 	i = 0;
-	len = ft_strlen(line);
+	len = ft_strlen(d->line);
 	while (i < len)
 	{
-		if (i < len && line[i] == '\"')
-			replace_dquote_loop(line, flags, &i, envp);
-		if (i < len && line[i] == '\'')
-			replace_squote_loop(line, flags, &i);
-		if (i < len && line[i] == '$' && line[i + 1] == '?')
-			get_exit_status_len(flags, &i, exit_status);
-		else if (i < len && line[i] == '$')
-			get_env_len(line, flags, &i, envp);
-		if (i < len && line[i] != '\"' && line[i] != '\'' && line[i] != '$')
+		if (i < len && d->line[i] == '\"')
+			replace_dquote_loop(&i, envp, d);
+		if (i < len && d->line[i] == '\'')
+			replace_squote_loop(d->line, d->flags, &i);
+		if (i < len && d->line[i] == '$' && d->line[i + 1] == '?')
+			get_exit_status_len(d->flags, &i, d->exit_status);
+		else if (i < len && d->line[i] == '$')
+			get_env_len(d->line, d->flags, &i, envp);
+		if (i < len && d->line[i] != '\"' && d->line[i] != '\'' && d->line[i] != '$')
 		{
-			flags[i] = 1;
+			d->flags[i] = 1;
 			i++;
 		}
 	}
@@ -97,19 +98,22 @@ static void	lexer_set_flags(char *line, int *flags, char **envp, int exit_status
 
 char	*format_cmd_line(char *line, char **envp, int exit_status)
 {
-	int		*flags;
-	char	*copy;
-	char	*new;
+	char			*new;
+	t_cmd_line_info	*d;
 
 	if (!line)
 		return (NULL);
-	flags = ft_calloc(ft_strlen(line), sizeof(int));
-	if (!flags && ft_perror())
+	d = malloc(sizeof(t_cmd_line_info));
+	if (!d && ft_perror())
 		return (NULL);
-	copy = ft_strdup(line);
-	if (!copy)
-		return (free(flags), NULL);
-	lexer_set_flags(copy, flags, envp, exit_status);
-	new = copy_flagged(copy, flags, envp, exit_status);
-	return (free(flags), free(copy), new);
+	d->flags = ft_calloc(ft_strlen(line), sizeof(int));
+	if (!d->flags && ft_perror())
+		return (NULL);
+	d->line = ft_strdup(line);
+	if (!d->line && ft_perror())
+		return (free(d->flags), free(d), NULL);
+	d->exit_status = exit_status;
+	lexer_set_flags(envp, d);
+	new = copy_flagged(d->line, d->flags, envp, d->exit_status);
+	return (free(d->flags), free(d->line), free(d), new);
 }
