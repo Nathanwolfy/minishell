ffda6c106ad78d63229c01417c773d1aae22aa71
Ellio Hickman <ehickman@k0r1p2.42mulhouse.fr>
2024-02-16 14:44:26 +0100

    quotes are now handled properly

--- srcs/lexing/lexing_utils.c
+++ srcs/lexing/lexing_utils.c
@@ -6,7 +6,7 @@
 /*   By: nlederge <nlederge@student.42mulhouse.f    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/11 18:38:21 by nlederge          #+#    #+#             */
-/*   Updated: 2024/02/12 15:34:23 by nlederge         ###   ########.fr       */
+/*   Updated: 2024/02/16 14:07:48 by ehickman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,10 +45,18 @@ int	print_error_lexer(int code, int *exit_status)
 
 int	simple_quotes_loop(char *line, int *k, int *table)
 {
+	table[*k] = -1;
+	*k += 1;
 	while (line[*k] && line[*k] != SQUOTE)
-		table[(*k)++] = 1;
+	{
+		table[(*k)] = 1;
+		*k += 1;
+	}
 	if (line[*k] == SQUOTE)
-		table[(*k)++] = 1;
+	{
+		table[*k] = -1;
+		*k += 1;
+	}
 	else if (!line[*k])
 		return (-1);
 	return (0);
@@ -56,10 +64,18 @@ int	simple_quotes_loop(char *line, int *k, int *table)
 
 int	double_quotes_loop(char *line, int *k, int *table)
 {
+	table[*k] = -1;
+	*k += 1;
 	while (line[*k] && line[*k] != DQUOTE)
-		table[(*k)++] = 1;
+	{
+		table[(*k)] = 1;
+		*k += 1;
+	}
 	if (line[*k] == DQUOTE)
-		table[(*k)++] = 1;
+	{
+		table[*k] = -1;
+		*k += 1;
+	}
 	else if (!line[*k])
 		return (-2);
 	return (0);
@@ -74,13 +90,11 @@ int	quote_sequence(int *table, char *line)
 	{
 		if (line[k] == SQUOTE)
 		{
-			table[k++] = 1;
 			if (simple_quotes_loop(line, &k, table) < 0)
 				return (-1);
 		}
 		else if (line[k] == DQUOTE)
 		{
-			table[k++] = 1;
 			if (double_quotes_loop(line, &k, table) < 0)
 				return (-2);
 		}
